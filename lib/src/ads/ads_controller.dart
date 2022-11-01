607126418a6a7e1aab4d477b327bcc5189e2787f
David Li <davidli012345@gmail.com>
2022-11-01 08:25:55 -0700

    Update ads_controller.dart

--- lib/src/ads/ads_controller.dart
+++ lib/src/ads/ads_controller.dart
@@ -37,7 +37,7 @@ class AdsController {
     // TODO: When ready, change this to the Ad Unit IDs provided by AdMob.
     //       The current values are AdMob's sample IDs.
     final adUnitId = defaultTargetPlatform == TargetPlatform.android
-        ? 'ca-app-pub-3940256099942544/6300978111'
+        ? 'ca-app-pub-2479144310234386~4409447550'
         // iOS
         : 'ca-app-pub-3940256099942544/2934735716';
     _preloadedAd =
